08208c27af9fa085a2ce077330fb841c3bec8f58
Rajesh <Rajesh.b1419@gmail.com>
2025-04-16 19:12:21 -0700

    changes to submodule

---
+++ src/trades/main.py
@@ -0,0 +1,60 @@
+# Create an Application instance with Kafka configs
+from quixstreams import Application
+from kraken_api import krakenAPI, Trade
+from loguru import logger
+
+def run(
+    kafka_broker_address: str,
+    kafka_topic_name: str,
+    kraken_api: krakenAPI
+):
+
+
+    app = Application(
+        broker_address=kafka_broker_address, 
+       )
+
+    # Define a topic "my_topic" with JSON serialization
+    topic = app.topic(name=kafka_topic_name, value_serializer='json')
+
+    # Create a Producer instance
+    with app.get_producer() as producer:
+
+        while True:
+            
+            events: list[Trade]=kraken_api.get_trades()
+
+            # 1. fetch the event from external API
+            #event = {"id": "1", "text": "Lorem ipsum dolor sit amet"}
+            for event in events:
+
+                # 2. Serialize an event using the defined Topic 
+                message = topic.serialize(
+                    #key=event["id"], 
+                    value=event.to_dict())
+
+                # 3. Produce a message into the Kafka topic
+                producer.produce(
+                    topic=topic.name, 
+                    value=message.value, 
+                    #key=message.key
+                )
+
+                logger.info(f'Produced message to topic {kafka_topic_name}: {message.value}')
+            
+
+
+             
+
+
+
+if __name__ == "__main__":
+
+# create object that can talk to kraken API and get trade data in real time
+    api=krakenAPI(product_ids=["BTC/EUR"])
+
+    run(
+        kafka_broker_address='localhost:31234',
+        kafka_topic_name='trades',
+        kraken_api=api
+    )
